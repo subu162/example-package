dbf543833c899df644c93c4f25025d79b5525f61
shubharanshu <spanda@gainsight.com>
2021-03-20 22:17:06 +0530

    first commit

---
+++ src/main/java/example/example.java
@@ -0,0 +1,13 @@
+package example;
+
+/**
+ * @author shubharanshupanda
+ * @created 20/03/21
+ * @project example-package
+ */
+public class example {
+
+    public static void display(String param) {
+        System.out.println("the passed param : "+param);
+    }
+}
